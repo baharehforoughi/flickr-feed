2d28e05a1f9d1697cc5fdd8581d94d5a7d594506
baharehforoughi <bhr.foroughi@gmail.com>
2024-04-30 23:33:40 +0200

    Update NavBar component with search functionality

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import Link from "next/link";
+
+import { useState } from "react";
 import { TfiFlickrAlt } from "react-icons/tfi";
 
 const NavBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && searchInput.trim() !== "") {
+      window.location.href = `/feeds/${searchInput}`;
+    }
+  };
   const links = [
-    { lable: "You", href: "/" },
-    { lable: "Explore", href: "/" },
-    { lable: "Prints", href: "/" },
-    { lable: "Get Pro", href: "/" },
+    { lable: "You", href: "/feeds/cat" },
+    { lable: "Explore", href: "/feeds/nature" },
+    { lable: "Prints", href: "/feeds/graphity" },
+    { lable: "Get Pro", href: "/feeds/dslr" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center bg-black">
@@ -26,6 +35,15 @@ const NavBar = () => {
           </Link>
         ))}
       </ul>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        className=" bg-neutral-300 px-3 py-1 rounded-md "
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        onKeyDown={handleSearch}
+      />
     </nav>
   );
 };
